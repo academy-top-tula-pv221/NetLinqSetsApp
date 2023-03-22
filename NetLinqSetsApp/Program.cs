6ccee41fb3454364f0688d2fb17ab025a577a19f
maxefimov <efimov.academy.top@gmail.com>
2023-03-22 20:05:53 +0300

    Добавьте файлы проекта.

---
+++ NetLinqSetsApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using ExampleClassLibrary;
+using System;
+
+var users1 = new List<User>()
+{
+    new("Sam", 35),
+    new("Bobby", 31),
+    new("Timophey", 29),
+    new("Joe", 42),
+    new("Leopold", 35),
+};
+
+foreach (var user in users1)
+    Console.WriteLine($"User name: {user.Name}, age: {user.Age}");
+Console.WriteLine();
+
+var users2 = new List<User>()
+{
+    new("Sam", 35),
+    new("Bob", 31),
+    new("Tim", 29),
+    new("Joe", 42),
+    new("Leo", 35),
+};
+foreach (var user in users1)
+    Console.WriteLine($"User name: {user.Name}, age: {user.Age}");
+Console.WriteLine();
+
+var usersUnion = users1.Union(users2).Distinct();
+
+Console.WriteLine("Users Union:");
+foreach (var user in usersUnion)
+    Console.WriteLine($"User name: {user.Name}, age: {user.Age}");
+Console.WriteLine();
+
+
+var usersIntersect = users1.Intersect(users2);
+
+Console.WriteLine("Users Intersect:");
+foreach (var user in usersIntersect)
+    Console.WriteLine($"User name: {user.Name}, age: {user.Age}");
+Console.WriteLine();
+
+var usersExcept = users1.Except(users2);
+
+Console.WriteLine("Users Except:");
+foreach (var user in usersExcept)
+    Console.WriteLine($"User name: {user.Name}, age: {user.Age}");
+Console.WriteLine();
+
+
+
